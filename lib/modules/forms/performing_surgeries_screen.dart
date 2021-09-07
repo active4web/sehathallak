132fc0e6ad58acead4446ea2dbb7fc67949549be
Mohab Sarhan <mohab.sarhan17@users.noreply.github.com>
2021-09-07 17:30:00 +0200

    finish surgery screen

--- lib/modules/forms/performing_surgeries_screen.dart
+++ lib/modules/forms/performing_surgeries_screen.dart
@@ -21,48 +21,93 @@ class PerformSurgery extends StatelessWidget {
       appBar: customAppBar(title: "اجراء العمليات الجراحية "),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            textFieldWithTitle(
-                title: "اسم صاحب المزرعة",
-                hintText: "يتم كتابة الاسم تلقائي في حالة الاشتراك المسبق ",
-                controller: farmName),
-            textFieldWithTitle(
-                title: "رقم الموبيل",
-                hintText: "يتم كتابة الموبيل تلقائي في حالة الاشتراك المسبق",
-                controller: phoneNumber),
-            textFieldWithTitle(
-                title: "رقم  حساب صحة حلالك",
-                hintText: "يتم كتابة رقم الحساب تلقائي في حالة الاشتراك المسبق",
-                controller: accountNumber),
-            dropDownMenuWithTitle(
-                hintText: "حدد",
-                value: animalType,
-                title: "نوع الحيوان / الطير ",
-                items: [],
-                onChanged: (value) {}),
-            Row(
-              children: [
-                Expanded(
-                  child: textFieldWithTitle(
-                      title: "عدد الحيوانات ",
-                      hintText: "اضف العدد",
-                      controller: animalsNumber),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              textFieldWithTitle(
+                  title: "اسم صاحب المزرعة",
+                  hintText: "يتم كتابة الاسم تلقائي في حالة الاشتراك المسبق ",
+                  controller: farmName),
+              textFieldWithTitle(
+                  title: "رقم الموبيل",
+                  hintText: "يتم كتابة الموبيل تلقائي في حالة الاشتراك المسبق",
+                  controller: phoneNumber),
+              textFieldWithTitle(
+                  title: "رقم  حساب صحة حلالك",
+                  hintText:
+                      "يتم كتابة رقم الحساب تلقائي في حالة الاشتراك المسبق",
+                  controller: accountNumber),
+              dropDownMenuWithTitle(
+                  hintText: "حدد",
+                  value: animalType,
+                  title: "نوع الحيوان / الطير ",
+                  items: [],
+                  onChanged: (value) {}),
+              Row(
+                children: [
+                  Expanded(
+                    child: textFieldWithTitle(
+                        title: "عدد الحيوانات ",
+                        hintText: "اضف العدد",
+                        controller: animalsNumber),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: dropDownMenuWithTitle(
+                        hintText: "المدينة/ الامارة",
+                        value: country,
+                        title: "",
+                        items: [],
+                        onChanged: (value) {}),
+                  ),
+                ],
+              ),
+              textFieldWithTitle(
+                  title: "نوع الجراحة المطلوبة",
+                  hintText: "نوع الجراحة المطلوبة",
+                  controller: surgeryType),
+              textFieldWithTitle(
+                  title: "الموعد المناسب للكشف",
+                  hintText: "الموعد المناسب للكشف",
+                  controller: surgeryTime),
+              textFieldWithTitle(
+                  title: "العنوان بالتفصيل",
+                  hintText: "العنوان بالتفصيل",
+                  controller: description),
+              TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
+                      color: kAppSecondColor,
+                    ),
+                    Text(
+                      "حدد العنوان على الخريطة",
+                      style: TextStyle(color: kAppSecondColor),
+                    )
+                  ],
                 ),
-                SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: dropDownMenuWithTitle(
-                      hintText: "المدينة/ الامارة",
-                      value: country,
-                      title: "",
-                      items: [],
-                      onChanged: (value) {}),
-                ),
-              ],
-            )
-          ],
+              ),
+              textFieldWithTitle(
+                  title: "نبذة عن الحالة المرضية أو الغرض من الكشف",
+                  hintText: "الشكوي المرضية التي يعاني منها الحيوان",
+                  maxLines: 5,
+                  controller: examinationPurpose),
+              textFieldWithTitle(
+                  title: "ملاحظات أخري",
+                  hintText: "الشكوي المرضية التي يعاني منها الحيوان",
+                  maxLines: 5,
+                  controller: otherNotes),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                child: customButton(text: "ارسال الطلب", onPressed: () {}),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -106,14 +151,11 @@ class PerformSurgery extends StatelessWidget {
               fontSize: 15,
             ),
           ),
-          Container(
-            height: 50,
-            child: CustomDropDownMenu(
-              hintText: hintText,
-              value: value,
-              items: items,
-              onChange: onChanged,
-            ),
+          CustomDropDownMenu(
+            hintText: hintText,
+            value: value,
+            items: items,
+            onChange: onChanged,
           ),
         ],
       );
@@ -128,7 +170,6 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
       child: TextField(
         controller: controller,
         maxLines: maxLines,
@@ -160,11 +201,11 @@ class CustomDropDownMenu extends StatelessWidget {
       ),
       child: DropdownButtonHideUnderline(
         child: Container(
-          height: 50,
           child: DropdownButton(
-              itemHeight: 50,
               value: value,
               isDense: true,
+              itemHeight: null,
+              isExpanded: true,
               hint: Text(
                 hintText,
                 style: TextStyle(fontSize: 12, color: kScaffoldColor),
