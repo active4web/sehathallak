7d9de9498fbbada75325e041cfd411b9182dc5a7
esam <esam.smesm@gmail.com>
2021-09-07 13:23:23 +0200

    Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:se7a_7alalk/modules/authintication/login_screen.dart';
 import 'package:se7a_7alalk/modules/authintication/register_screen.dart';
 import 'package:se7a_7alalk/modules/layout/home_layout.dart';
+import 'package:se7a_7alalk/modules/screens/home_screen.dart';
 import 'package:se7a_7alalk/shared/cubit/app_cubit.dart';
 
 void main() {
@@ -17,15 +18,17 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => AppCubit()),
       ],
       child: MaterialApp(
-        initialRoute: LoginScreen.id,
+        initialRoute: HomeLayout.id,
         routes: {
           LoginScreen.id: (context) => LoginScreen(),
           RegisterScreen.id: (context) => RegisterScreen(),
           HomeLayout.id: (context) => HomeLayout(),
+          HomeScreen.id: (context) => HomeScreen(),
         },
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          fontFamily: 'Cairo'
         ),
         builder: (context, child) {
           return Directionality(textDirection: TextDirection.rtl, child: child);
---
+++ lib/modules/screens/home_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:se7a_7alalk/shared/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  static const String id = "HomeScreen";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor:  Color(0xffF8F8F8),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text('عن التطبيق',style: TextStyle(
+          color: Colors.white,
+        ),),
+        centerTitle: true,
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  gradient: kAppGradient,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(12),
+                    bottomRight: Radius.circular(12),
+                  )
+                ),
+                height: MediaQuery.of(context).size.height*0.6,
+              )
+            ],
+          ),
+          Positioned(
+            width: 130,
+            height: 130,
+            top: 130,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/logo.png'),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 280,
+            height: MediaQuery.of(context).size.height*0.5,
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('عن التطبيق',style: TextStyle(
+                          color: kAppColor,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold
+                        ),),
+                      ],
+                    ),
+                    Container(
+                      child: Text(kAboutAppText,style: TextStyle(
+                        fontSize: 13,
+                        color: kAppColor
+                      ),),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
